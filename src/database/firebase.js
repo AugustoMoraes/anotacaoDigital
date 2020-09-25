3f012d6b020c30a3342f79004653e3687c967c2e
Augusto Moraes <devaugusto.moraes@gmail.com>
2020-09-24 23:10:24 -0300

    criado banco de dados de teste para essa aplicação

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,7 +1,21 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
-var firebaseConfig = {
+
+var firebaseConfig = { //Banco de Dados Teste
+  apiKey: "AIzaSyC4gMIGQr9q__vnY05hbXsU5_64HcgrVJ4",
+  authDomain: "anotacaodigitalteste.firebaseapp.com",
+  databaseURL: "https://anotacaodigitalteste.firebaseio.com",
+  projectId: "anotacaodigitalteste",
+  storageBucket: "anotacaodigitalteste.appspot.com",
+  messagingSenderId: "1040302330708",
+  appId: "1:1040302330708:web:1043633ab1ff07d3286ec1",
+  measurementId: "G-T6W9D45B4S"
+};//Banco de Dados Teste
+
+
+/** 
+var firebaseConfig = { // Banco de Dados Edivaldo (Cigarreiro)
   apiKey: "AIzaSyCxvzvteiFBy7xC8xhjp0twFgQ3ex31kyw",
   authDomain: "anotacaodigital-616e7.firebaseapp.com",
   databaseURL: "https://anotacaodigital-616e7.firebaseio.com",
@@ -10,7 +24,10 @@ var firebaseConfig = {
   messagingSenderId: "670056731435",
   appId: "1:670056731435:web:f744254ff51ab3f6ab2b6e",
   measurementId: "G-KVW7PHQ900"
-};
+}; // Banco de Dados Edivaldo (Cigarreiro)
+*/
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
