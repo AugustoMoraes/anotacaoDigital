0409d783beb6a8f922c0a2a4201c226ecf311305
Augusto Moraes <devaugusto.moraes@gmail.com>
2022-01-22 10:49:24 -0300

    comentario de teste na rota

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Clientes from './pages/Clientes'
 import Produtos from './pages/Produtos'
@@ -12,24 +12,25 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 Ionicons.loadFont()
 const Tab = createBottomTabNavigator()
 const Stack = createStackNavigator()
-function Tabs(){
-  return(
+function Tabs() {
+  return (
     <Tab.Navigator tabBarOptions={{
       activeTintColor: '#fff',
       inactiveTintColor: '#000',
       activeBackgroundColor: '#999',
       showLabel: false,
     }}>
-      <Tab.Screen name="Clientes" component={Clientes} options={{
-          tabBarIcon:()=>(
-              <Ionicons name='people' size={50} color='#000' />
-          )
-      }}/>
-      <Tab.Screen name="Produtos" component={Produtos} options={{
-        tabBarIcon: ()=>(
-          <Ionicons name='cart' size={50} color='#000'/>
+      //Teste git 
+      <Tab.Screen name="Clientes" component={Clientes} options={{ 
+        tabBarIcon: () => (
+          <Ionicons name='people' size={50} color='#000' />
         )
-      }}/>
+      }} />
+      <Tab.Screen name="Produtos" component={Produtos} options={{
+        tabBarIcon: () => (
+          <Ionicons name='cart' size={50} color='#000' />
+        )
+      }} />
     </Tab.Navigator>
   )
 }
@@ -39,9 +40,9 @@ export default function Routes() {
       <Stack.Navigator screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="Clientes" component={Tabs}/>
-        <Stack.Screen name="ListDividaCliente" component={ListDividaCliente}/>
-        <Stack.Screen name="AvCliente" component={AvCliente}/>
+        <Stack.Screen name="Clientes" component={Tabs} />
+        <Stack.Screen name="ListDividaCliente" component={ListDividaCliente} />
+        <Stack.Screen name="AvCliente" component={AvCliente} />
       </Stack.Navigator>
     </NavigationContainer>
   );
